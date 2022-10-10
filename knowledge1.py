8d406a8597101eb3f815da28b6da304706dbcb00
Beth Hart <beth.hart89@gmail.com>
2022-10-09 23:38:34 -0400

    Knowledge 1 Initial Commit

---
+++ knowledge1.py
@@ -0,0 +1,7 @@
+print("hello world!")
+boy_names = ["Matt", "Robert", "James", "Jeff"]
+print(boy_names[0])
+item_prices = {"item1":2, "item2":5, "item3":10}
+print(item_prices["item1"])
+girl_names = ("Jean", "Riley", "Olivia", "Robin")
+print(girl_names[1])
